4e65cdc3d421821bbb23762dc046e36860a417eb
lf-xxxyyy <liufeng.lf@aliyun.com>
2017-01-26 10:30:42 +0800

    say hi

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
@@ -6,11 +7,19 @@ from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
+from flask_sqlalchemy import SQLAlchemy
 
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+	'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db = SQLAlchemy(app)
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
@@ -19,22 +28,23 @@ moment = Moment(app)
 def index():
 	form = NameForm()
 	if form.validate_on_submit():
-		old_name = session.get('name')
-		if old_name is not None and old_name != form.name.data:
-			flash('looks like you have changed your name!')
+		user = User.query.filter_by(username=form.name.data).first()
+		if user is None :
+			user = User(username = form.name.data)
+			db.session.add(user)
+			db.session.commit()
+			session['known'] = False
+		else :
+			session['known'] = True
 		session['name'] = form.name.data
+		form.name.data = ''
 		return redirect(url_for('index'))
-	return render_template('index.html', form=form, name = session.get('name'))
+	return render_template('index.html', form=form, name = session.get('name'), known=session.get('known', False))
+
 
 @app.route('/user/<name>/')
 def user(name):
 	return render_template('user.html', name=name)
-
-
-@app.route('/testlist/')
-def renderList():
-	La = [1,2,3,4]
-	return render_template('user.html', La=La)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -43,6 +53,45 @@ def page_not_found(e):
 class NameForm(FlaskForm):
 	name = StringField('what is your name?', validators=[Required()])
 	submit = SubmitField('Submit')
+
+
+
+
+class Role(db.Model):
+	__tablename__ ="roles"
+	id = db.Column(db.Integer, primary_key = True)
+	name = db.Column(db.String(64), unique= True)
+	users = db.relationship('User', backref='role')
+
+	def __repr__(self):
+		return '<Role %r>' % self.name
+
+class User(db.Model):
+	__tablename__ = "users"
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(64), unique=True, index=True)
+	role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+	def __repr__(self):
+		return '<User %r>' % self.username
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
